d022f7069fc1bbec9e3c07c6e2ebe217455216b1
Nikita Postnov <117923446+NikitaPostnov101@users.noreply.github.com>
2023-07-27 11:22:52 +0300

    Add files via upload

---
+++ Characters.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+
+def count_characters():
+    text = text_entry.get("1.0", tk.END)
+    num_characters = len(text) - 1  # Исключаем символ новой строки в конце текста
+    result_label.config(text=f"Количество символов: {num_characters}")
+
+# Создаем окно
+root = tk.Tk()
+root.title("Считывание количества символов")
+
+# Создаем элементы интерфейса
+text_entry = tk.Text(root, wrap=tk.WORD, height=10, width=40)
+text_entry.pack(pady=10)
+
+count_button = tk.Button(root, text="Посчитать", command=count_characters)
+count_button.pack()
+
+result_label = tk.Label(root, text="", font=("Helvetica", 14))
+result_label.pack(pady=10)
+
+# Запускаем главный цикл обработки событий
+root.mainloop()
